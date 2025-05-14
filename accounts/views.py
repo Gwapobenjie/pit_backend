d79525b9a253d4352943f8fe0a5ad2a1b5eec300
Gwapobenjie <plazabenjie1@gmail.com>
2025-05-15 02:12:11 +0800

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework.views import APIView
+from django.contrib.auth import login
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            login(request, user)
+            return Response({"message": "Login successful"})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
